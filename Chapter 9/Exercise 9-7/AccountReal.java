e8e14d7adfa498fabf01a4eb5dca9651c186ff9d
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-10-22 18:41:24 -0600

    Corrected some misunderstandings

--- Chapter 9/Exercise 9-7/AccountReal.java
+++ Chapter 9/Exercise 9-7/AccountReal.java
@@ -1,36 +1,55 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class AccountReal {
 	
+	private Scanner input = new Scanner(System.in);
 	private int id;
 	private double balance;
 	private double annualInterestRate;
-	private java.util.Date dateCreated = new java.util.Date();
+	private java.util.Date dateCreated;
 	
 	
 	AccountReal() {
 		id = 0;
 		balance = 0;
 		annualInterestRate = 0;
+		dateCreated = new java.util.Date();
 	}
 	
 	AccountReal(int newID, double newBalance, double newAnnualInterestRate) {
 		id = newID;
 		balance = newBalance;
 		annualInterestRate = newAnnualInterestRate;
+		dateCreated = new java.util.Date();
 		
+	}
+	
+	double setBalance() {
+		balance = input.nextDouble();
+		return balance;
+	}
+	
+	int setID() {
+	id = input.nextInt();
+	return id;
+	}
+	
+	double setAnnualInterestRate() {
+		annualInterestRate = input.nextDouble();
+		return annualInterestRate;
 	}
 	
 	int getID() {
 		return id;
 	}
 	
-	double getWithdraw(double amount) {
-		return balance - amount;
+	public double Withdraw(double amount) {
+		return balance -= amount;
 	}
 	
-	double getDeposit(double amount) {
-		return balance - amount;
+	public double Deposit(double amount) {
+		return balance += amount;
 	}
 	
 	double getBalance() {
--- Chapter 9/Exercise 9-7/Exercise_07_09.java
+++ Chapter 9/Exercise 9-7/Exercise_07_09.java
@@ -1,16 +1,20 @@
-
+/*
+ * Matthew Shelley
+ * 10/17/2019
+ * Exercise 7-9 Does some bank like stuff.
+ */
 public class Exercise_07_09 {
 
 	public static void main(String[] args) {
 		// Calls the default account
-		
+
 		AccountReal account1 = new AccountReal();
 		System.out.println("The default account id is " + account1.getID());
 		System.out.println("The default account balance is " + account1.getBalance());
 		System.out.println("The default account monthly interest is " + account1.getMonthlyInterest());
 		System.out.println("The date the default account was created was " + account1.getDate());
 		System.out.println("\n");
-		
+
 		AccountReal account2 = new AccountReal(1122, 20000, 4.5);
 		account2.getWithdraw(2500);
 		account2.getDeposit(3000);
@@ -19,5 +23,5 @@ public class Exercise_07_09 {
 		System.out.println("The account monthly interest is " + account2.getMonthlyInterest());
 		System.out.println("The date the saccount was created was " + account2.getDate());
 	}
-	
+
 }
